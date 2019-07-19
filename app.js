88f0b49e5557b30b905fc5f52be9eb695d8d5c3f
choijungjin1 <cjj1002@naver.com>
2019-07-19 17:58:23 +0900

    a_smallproject01 commit

--- app.js
+++ app.js
@@ -6,12 +6,36 @@ const port = 3000
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.use(express.static('public'))
+
 const path = require('path');
 const router = express.Router();
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-
+router.get('/a_homepage', function (req, res) {
+    res.sendFile(path.join(__dirname + '/view/a_homepage.html'));
+});
+router.get('/a_notice', function (req, res) {
+    res.sendFile(path.join(__dirname + '/view/a_notice.html'));
+});
+router.get('/a_signup', function (req, res) {
+    res.sendFile(path.join(__dirname + '/view/a_signup.html'));
+});
+router.get('/homepage_search', function (req, res) {
+    console.log(req.query);
+    res.send('Request parameters : '+req.query.search)
+});
+router.get('/notice_question', function (req, res) {
+    console.log(req.query);
+    res.send('Request parameters : '+req.query.question)
+});
+router.get('/notice_write', function (req, res) {
+    console.log(req.query);
+    res.send('Request parameters : '+req.query.write)
+});
+router.get('/signup', function (req, res) {
+    console.log(req.query);
+    res.send('Request parameters : '+req.query.name+', '+req.query.birth+','+req.query.id+','+req.query.pwd+','+req.query.email)
+});
 app.use('/', router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
